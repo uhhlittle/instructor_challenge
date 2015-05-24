d2734817ada78a0d083005846f450b63ae4b6172
uhhlittle <allysonjlittle@gmail.com>
2015-05-24 17:45:31 -0400

    render click result w/info in list. favorite button added

--- public/js/app.js
+++ public/js/app.js
@@ -7,31 +7,55 @@
     var xhr = new XMLHttpRequest();
     xhr.open('get', 'http://omdbapi.com/?s=' + encodeURIComponent(search_term), true);
     xhr.addEventListener('load', function(response){
+      console.log(response);
       var res =  JSON.parse(this.response).Search;
-      document.querySelector('ul').innerHTML = "";
+      console.log(res);
+        document.querySelector('ul').innerHTML = "";
       for(var i = 0; i < res.length; i++){
         var node = document.createElement('li');
         var title = res[i].Title;
-        node.id = res[i].imdbID
+        node.id = res[i].imdbID;
+        node.title = res[i].Title;
+        node.year = res[i].Year;
+        node.genre = res[i].Genre;
+        node.director = res[i].Director;
+        node.actors = res[i].Actors;
         node.innerHTML = title;
         document.querySelector('ul').appendChild(node);
+        // make nodes clickable
         node.addEventListener('click', function(event){
-          console.log("you clicked a node");
-          console.log(this.id);
-          // var ID = res[i].imdbID;
+          var movie = document.createElement('p');
+          movie.innerHTML = this.title;
+          var movieYear = document.createElement('p');
+          movieYear.innerHTML = this.year;
+            var btn = document.createElement("BUTTON");        // Create a <button> element
+            var t = document.createTextNode("FAVORITE");       // Create a text node
+            btn.appendChild(t);
+
+            document.querySelector('ul').appendChild(movie);
+            document.querySelector('ul').appendChild(movieYear);
+            document.querySelector('ul').appendChild(btn);
+
+
+            // var id = this.id
           // var idRequest = new XMLHttpRequest();
-          // idRequest.open('get', 'http://omdbapi.com/?s' + encodeURIComponent(ID), true);
-          //   idRequest.addEventListener('load', function(response){
-          //     // var individualRes = JSON.parse(this.reponse).Search
+          // idRequest.open('get', 'http://omdbapi.com/?s' + encodeURIComponent(id), true);
+          // idRequest.addEventListener('load', function(response){
+          //     var individualRes = JSON.parse(this.reponse).Search;
+          //     console.log(individualRes);
           //     var movieAttributes = [];
-          //     var title = this.Title;
-          //     var rating = this.Rated;
-          //     var year = this.Year;
+          //     var title = individualRes.Title;
+          //     var rating = individualRes.Rated;
+          //     var year = individualRes.Year;
           //     movieAttributes << title;
           //     movieAttributes << rating;
           //     movieAttributes << year;
           //
-          //     for(var i=0; i < movieAttribues.length; i++)
+          //     for(var i=0; i < movieAttribues.length; i++){
+          //       listing = document.createElement('p');
+          //       listing.innerHTML = movieAttributes[i];
+          //       document.querySelector('li').appendChild(listing);
+          //     }
           //   })
         })
       }
